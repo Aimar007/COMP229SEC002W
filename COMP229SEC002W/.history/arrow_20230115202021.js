3343b47d5316c8cea1bc3bb7b68a0acb6a3ae8ab
Aimar007 <aimar.htv@gmail.com>
2023-01-16 21:22:38 -0500

    initial commit

---
+++ COMP229SEC002W/.history/arrow_20230115202021.js
@@ -0,0 +1,20 @@
+/*
+function sum1(a,b){
+    return a + b;
+}
+let result = sum1(1,2);
+console.log(result);
+*/
+
+/*
+const sum1 = (a, b) =>{
+    return a + b;
+}
+let result = sum1(2,2);
+console.log(result);
+*/
+
+const sum1 = (a, b) => a + b;
+    return a + b;
+let result = sum1(2,2);
+console.log(result);
